10200df9cc8ce54009966a704ffc1f4308872660
supremojvy25 <yambao.jimmel@gmail.com>
2026-03-06 16:55:24 +0800

    Initial commit

---
+++ product-api/app/Http/Controllers/ProductController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return Product::all();
+    }
+
+    public function store(Request $request)
+    {
+        return Product::create($request->all());
+    }
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::findOrFail($id);
+        $product->update($request->all());
+
+        return $product;
+    }
+
+    public function destroy($id)
+    {
+        Product::destroy($id);
+        return response()->json(['message' => 'Deleted']);
+    }
+}
